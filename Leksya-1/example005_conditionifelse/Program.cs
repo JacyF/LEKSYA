35469b2d18a549ac736ec3efb236fd5f3c286486
JacyF <brunoloouuiis@gmail.com>
2022-11-15 01:28:18 +0300

    added example005_conditionifelse

---
+++ Leksya-1/example005_conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if ( username.ToLower() == "bruno" )
+{
+    Console.WriteLine(" Ура, это BRUNO");
+}
+else
+{
+    Console.WriteLine(" Привет , ");
+    Console.WriteLine(username);
+}

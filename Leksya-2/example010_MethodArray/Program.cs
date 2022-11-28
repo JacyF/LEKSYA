67a9927502d17e1abf2843ed084573fafe6a7ea7
JacyF <brunoloouuiis@gmail.com>
2022-11-28 15:12:34 +0300

    added leksya -2

---
+++ Leksya-2/example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {22,44,56,85,90,44,43,64,23,45,};
+
+int n = array.Length;
+
+int find = 44 ;
+
+int index = 0 ;
+
+while (index < n )
+{
+    if (array[index] == find )
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+} 

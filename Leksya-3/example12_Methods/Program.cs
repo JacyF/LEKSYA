1f636eb121292d8873d7d0486efcd12826b0eb1d
JacyF <brunoloouuiis@gmail.com>
2022-12-04 21:01:43 +0300

    excercise leks

--- Leksya-3/example12_Methods/Program.cs
+++ Leksya-3/example12_Methods/Program.cs
@@ -2,7 +2,7 @@
 
 void Method1()
 {
-    Console.Write(" Hi , Mister Bruno ") ;
+    Console.Write(" Hi , Mister Bruno ");
 }
 // Method1();
 
@@ -11,16 +11,16 @@ void Method1()
 
 void Method2(string msg)
 {
-    Console.Write(msg) ;
+    Console.Write(msg);
 }
 // Method2(msg : " Text ");
 
 void Method21(string msg, int count)
 {
-    int i = 0 ;
-    while (i < count )
+    int i = 0;
+    while (i < count)
     {
-        Console.Write(msg) ;
+        Console.Write(msg);
         i++;
     }
 }
@@ -30,7 +30,7 @@ void Method21(string msg, int count)
 
 int Method3()
 {
-    return DateTime.Now.Year ;
+    return DateTime.Now.Year;
 }
 // int year = Method3();
 // Console.Write(year);
@@ -49,13 +49,13 @@ int Method3()
 //     return result;
 // }
 
-string Method4(int count , string text)
+string Method4(int count, string text)
 {
- 
-    string result = string.Empty ;
-    for ( int i = 0 ; i < count ; i++ )
+
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
     {
-        result = result + text ;  
+        result = result + text;
     }
     return result;
 }
@@ -86,18 +86,86 @@ string text = "— Я думаю, — сказал князь, улыбаясь,
 
 string Replace(string text, char oldValue, char newValue)
 {
-    string result = String.Empty ;
-    int lenght = text.Length ;
-    for (int i = 0 ; i < lenght ; i++ )
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
     {
-        if (text[i] == oldValue )result = result + $"{newValue}" ;
-        else result = result + $" {text[i]}" ;
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $" {text[i]}";
     }
-    return result ;
+    return result;
 }
-string newText = Replace(text , ' ','|' );
-Console.WriteLine(newText);
+// string newText = Replace(text , ' ','|' );
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText , 'к','К' );
+// Console.WriteLine(newText);
+
+
+int[] arr = { 1, 4, 5, 8, 3, 5, 9, 1, 0 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int [] array)
+{
+    for ( int i = 0 ; i < array.Length - 1; i++)
+    {
+        int minPosition = i ;
+
+        for ( int j = i+1 ; j < array.Length ; j++)
+        {
+            if ( array[j] < array[minPosition]) minPosition = j ;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary ;
+    }
+}
+
+
+
+void MaxSelectionSort(int [] array)
+{
+    for ( int i = 0 ; i < array.Length - 1; i++)
+    {
+        int maxPosition = i ;
+
+        for ( int j = i+1 ; j < array.Length ; j++)
+        {
+            if ( array[j] > array[maxPosition]) maxPosition = j ;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary ;
+    }
+}
+
+
+Console.WriteLine(" Array : ");
+PrintArray(arr);
 Console.WriteLine();
-newText = Replace(newText , 'к','К' );
-Console.WriteLine(newText);
+
+Console.WriteLine(" From Min to Max ");
+SelectionSort(arr) ;
+PrintArray(arr);
+Console.WriteLine();
+
+Console.WriteLine(" From Max to Min ");
+MaxSelectionSort(arr) ;
+PrintArray(arr);
+
 
